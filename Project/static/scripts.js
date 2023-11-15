989b4f32d6042b70914780be57934fbc7912b851
kamVelo <alik87932@gmail.com>
2023-11-14 18:22:32 -0600

    created modal window

---
+++ Project/static/scripts.js
@@ -0,0 +1,9 @@
+
+function openModalWindow() {
+  let modalWindow = document.getElementsByClassName("modalContainer")[0];
+  modalWindow.style.display = "block";
+}
+function closeModalWindow() {
+  let modalWindow = document.getElementsByClassName("modalContainer")[0];
+  modalWindow.style.display = "none";
+}
